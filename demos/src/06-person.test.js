dfd47af43a536eeaec4b14e4cd8dfbffa615f8d7
Heladio Arteaga Franco <heladiofog@gmail.com>
2024-01-09 14:58:58 -0600

    AAA - Arrange, Act, Assert methodology/mantra.

--- demos/src/06-person.test.js
+++ demos/src/06-person.test.js
@@ -2,15 +2,18 @@ const Person = require('./06-person');
 
 describe('Testing the Person class', () => {
   let person;
-
+  // Arrange
   beforeEach(() => {
     person = new Person('Juli', 45, 1.7);
   });
 
   test('Should return IMC down', () => {
     // const person = new Person('Juli', 45, 1.7);
+    // Local arrange
     person.weight = 45;
+    // Act
     const imcObtained = person.calculateIMC();
+    // Assert
     expect(imcObtained).toBe('down');
   });
 
